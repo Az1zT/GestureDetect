49eaa1fca045a8dc2c64e816bc9c3351f28eb8f2
Az1zT <113937295+Az1zT@users.noreply.github.com>
2024-04-07 14:01:01 +0530

    Adding code to git

---
+++ tflite-model/model.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer, Dropout, Conv1D, Conv2D, Flatten, Reshape, MaxPooling1D, MaxPooling2D, BatchNormalization, TimeDistributed
+from tensorflow.keras.optimizers import Adam
+model = Sequential()
+model.add(Dense(20, activation='relu',
+    activity_regularizer=tf.keras.regularizers.l1(0.00001)))
+model.add(Dense(10, activation='relu',
+    activity_regularizer=tf.keras.regularizers.l1(0.00001)))
+model.add(Dense(classes, activation='softmax', name='y_pred'))
+opt = Adam(lr=0.0005, beta_1=0.9, beta_2=0.999)
+BATCH_SIZE = 32
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
+validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
+callbacks.append(BatchLoggerCallback(BATCH_SIZE, train_sample_count, epochs=100))
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+model.fit(train_dataset, epochs=100, validation_data=validation_dataset, verbose=2, callbacks=callbacks)
